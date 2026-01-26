ee0fea605c8d53f02d4a3728bb244a74131a9362
Madhu Mithra <madhumithraoffcl@gmail.com>
2026-01-26 20:28:14 +0530

    Implemented JWT authentication (register & login)

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -14,7 +15,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("E-Commerce API is running");
 });
-
+app.use("/api/auth", authRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
